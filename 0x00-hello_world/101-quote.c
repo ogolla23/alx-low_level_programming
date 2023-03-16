8fa7f9d2a9a31501adfde2fa480606467ce618cd
Leonard <leonardogolla23@gmail.com>
2023-03-16 05:17:44 -0700

    quote

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,5 +1,4 @@
 #include <stdio.h>
-
 /**
  * main - Entry point
  *
@@ -7,6 +6,6 @@
  */
 int main(void)
 {
-    fputs("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stderr);
-    return (1);
+fputs("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stderr);
+return (1);
 }
