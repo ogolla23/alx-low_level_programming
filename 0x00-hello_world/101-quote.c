5d59936cb43e5c909a78101d1e24aa2b9645b39d
Leonard <leonardogolla23@gmail.com>
2023-03-16 04:35:33 -0700

    quote

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -2,11 +2,11 @@
 /**
  * main - Entry point
  *
- * Return: Always 1 (Error)
+ * Return: 1 (Success)
  */
 int main(void)
 {
-    char message[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
-    write(2, message, sizeof(message) - 1);
-    return (1);
+char message[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+write(2, message, sizeof(message) - 1);
+return (1);
 }
