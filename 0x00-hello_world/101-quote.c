97acfac8bc98405664ac8b2faa054b23ff44637a
Leonard <leonardogolla23@gmail.com>
2023-03-16 04:32:24 -0700

    quote

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,12 +1,12 @@
-#include <stdio.h>
-#include <unistdio.h>
+#include <unistd.h>
 /**
  * main - Entry point
  *
- * Return: 1 (Success)
+ * Return: Always 1 (Error)
  */
 int main(void)
 {
-fput("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
-return (1);
+    char message[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+    write(2, message, sizeof(message) - 1);
+    return (1);
 }
