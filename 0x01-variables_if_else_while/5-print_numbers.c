b7fd2e16fff5a05ad16a9ac2d44fb5e93361d872
Leonard <leonardogolla23@gmail.com>
2023-03-17 06:20:36 -0700

    numbers list

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+*main -A program that writes numbers
+*Return:Always 0 (Success)
+*/
+
+int main(void)
+{
+int i;
+
+for (i = 0; i <= 9; i++)
+putchar(i + '0');
+putchar('\n');
+
+return (0);
+}
