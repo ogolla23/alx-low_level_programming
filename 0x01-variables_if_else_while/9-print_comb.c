60dd8fecb0f95b0a261718e28757f84178d3c9bc
Leonard <leonardogolla23@gmail.com>
2023-03-17 06:55:07 -0700

    list of numbers

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -3,22 +3,19 @@
 *main -Another program that writes combinations of single digits
 *Return:Always 0 (Success)
 */
+
 int main(void)
 {
-int i, j;
-
-for (i = 0; i <= 8; i++)
-{
-for (j = i + 1; j <= 9; j++)
+int i = 0;
+while (i <= 9)
 {
 putchar(i + '0');
-putchar(j + '0');
-if (i != 8 || j != 9)
+if (i != 9)
 {
 putchar(',');
 putchar(' ');
 }
+i++;
 }
-}
-putchar('\n');
+return (0);
 }
