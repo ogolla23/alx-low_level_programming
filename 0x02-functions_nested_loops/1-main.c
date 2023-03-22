ae5be380547adb333ebc195dd9b3b4d9b1068cd8
Leonard <leonardogolla23@gmail.com>
2023-03-22 07:53:52 -0700

    1-alphabet.c

--- 0x02-functions_nested_loops/1-main.c
+++ 0x02-functions_nested_loops/1-main.c
@@ -5,6 +5,6 @@
  */
 int main(void)
 {
-    print_alphabet();
+void print_alphabet(void);
     return (0);
 }
---
+++ 0x02-functions_nested_loops/4-main.c
@@ -0,0 +1,20 @@
+/**
+ * main - check the code.
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+    int _isalpha(int c);
+    
+    c = _isalpha('H');
+    _putchar(c + '0');
+    c = _isalpha('o');
+    _putchar(c + '0');
+    c = _isalpha(108);
+    _putchar(c + '0');
+    c = _isalpha(';');
+    _putchar(c + '0');
+    _putchar('\n');
+    return (0);
+}
