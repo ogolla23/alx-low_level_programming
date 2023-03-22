b010be304674a786e4f21a44d2721d1b5584ff25
Leonard <leonardogolla23@gmail.com>
2023-03-22 11:05:27 -0700

    3-islower.c

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -1,17 +1,15 @@
-#include"main.h"
+#include "main.h"
+
 /**
-* _islower - checks if character is lowercase
-* @c: is the char to be checked
-*Return: 1 if char is lowercase,otherwise 0
-*/
+ * _islower - check if char is lowercase
+ * @c: is the char to be checked
+ * Return: 1 if char is lowercase, otherwise 0.
+ */
+
 int _islower(int c)
-
 {
-if (c >= 97 && c <= 122)
-{
-	
-	return (1);
-}
-
-	return (0);
+	if (c >= 'a' && c <= 'z')
+		return (1);
+	else
+		return (0);
 }
--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -1,26 +1,25 @@
 #include "main.h"
+
 /**
-* print_sign - Prints the sign of a number
-* @n: The number to be checked
-* Return: 1 for positive,-1 or negative and zero for whatever
-*/
+ * print_sign - prints the sign of a number
+ * @n: the int to check
+ * Return: 1 and prints + if n is greater than zero
+ * 0 and prints 0 if n is zero
+ *-1 and prints - if n is less than zero
+ */
 int print_sign(int n)
-
 {
-
 	if (n > 0)
 	{
-	_putchar(43);
-	return (1);
-	}
-	else if (n < 0)
+		_putchar('+');
+		return (1);
+	} else if (n == 0)
+}
+		_putchar(48);
+		return (0);
+	} else if (n < 0)
 	{
-	_putchar(45);
-	return (-1);
+		_putchar('-');
 	}
-	else
-	{
-	_putchar(48);
-	return (0);
-	}
+		return (-1);
 }
