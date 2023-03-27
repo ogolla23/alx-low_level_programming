f0150960889bff46e8992432fb9e111c5908604f
Leonard <leonardogolla23@gmail.com>
2023-03-27 04:54:55 -0700

    0-reset_to_98.c

---
+++ 0x05-pointers_arrays_strings/_putchar.c
@@ -0,0 +1,11 @@
+#include "main.h"
+#include <unistd.h>
+/**
+* -putchar - writes the characters
+*Return: On success 1.
+*On error, -1 is returned, and errno is set appropriately.
+*/
+int _putchar(char c)
+{
+ return (write(1, &c, 1));
+}
