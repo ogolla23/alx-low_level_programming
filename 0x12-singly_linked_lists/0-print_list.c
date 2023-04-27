e47fd369101eb8ad36ccbdd259527cd077b57543
Leonard <leonardogolla23@gmail.com>
2023-04-27 03:10:06 -0700

    update 1.0 0-print_list.c

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -7,18 +7,18 @@
  */
 size_t print_list(const list_t *h)
 {
-	size_t s = 0;
+	size_t count = 0;
 
-	while (h)
+	while (h != NULL)
 	{
-		if (!h->str)
-			printf("[0] (nil)\n");
-		else
-			printf("[%u] %s\n", h->len, h->str);
-		h = h->next;
-		s++;
+	if (h->str == NULL)
+		printf("[0] (nil)\n");
+	else
+		printf("[%u] %s\n", h->len, h->str);
+
+	count++;
+	h = h->next;
 	}
 
-	return (s);
+	return (count);
 }
-
