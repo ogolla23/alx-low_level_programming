99a7c1c1e63f333f68ef0992ed6f3d76ed89467a
ogolla23 <leonardogolla23@gmail.com>
2023-08-21 01:15:13 -0700

    dynamic libraries tasks

---
+++ 0x18-dynamic_libraries/3-puts.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * _puts - outputs string folloed by new line
+ *@str:string
+ */
+void _puts(char *str)
+{
+	while (*str != '\0')
+	{
+		_putchar(*str++);
+	}
+		_putchar('\n');
+}
